33c7e218b98ec53ab84c9c41be1080f7da1095ee
Juliano Souza <46322485+thespamer@users.noreply.github.com>
2023-03-20 15:16:18 -0300

    Update deploy_docker.sh

--- scripts/github/deploy_docker.sh
+++ scripts/github/deploy_docker.sh
@@ -11,8 +11,8 @@ if [ "$1" = "dev" -o "$1" = "main" ]; then
     docker build -t $DOCKERHUB_PROJECT . --build-arg BUILDKIT_INLINE_CACHE=1
 else
     # Building tag version from staging image (vX.X.X)
-    docker pull neonlabsorg/$DOCKERHUB_PROJECT:staging && \
-    docker build -t $DOCKERHUB_PROJECT . --cache-from neonlabsorg/$DOCKERHUB_PROJECT:staging --build-arg BUILDKIT_INLINE_CACHE=1 || \
+    docker pull neonlabsorg/$DOCKERHUB_PROJECT:latest && \
+    docker build -t $DOCKERHUB_PROJECT . --cache-from neonlabsorg/$DOCKERHUB_PROJECT:latest --build-arg BUILDKIT_INLINE_CACHE=1 || \
     docker build -t $DOCKERHUB_PROJECT . --build-arg BUILDKIT_INLINE_CACHE=1
     # Only push latest on release
     case $1 in v*)
